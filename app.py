db656c9da769d7d94d0f10edce52e0a9752aef62
Dylan B <dylan@MacBook-Pro-D-2.local>
2023-05-02 12:19:06 +0200

    design5

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 import requests
+from bs4 import BeautifulSoup
 import openai
 import random
 import os
@@ -10,13 +11,11 @@ app = Flask(__name__)
 # Load OpenAI API key from environment variables
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
+
 def scrape_painting():
     api_key = os.environ.get("HARVARD_API_KEY")
     url = f"https://api.harvardartmuseums.org/object?apikey={api_key}&size=100&sort=random&classification=Paintings&hasimage=1&sortorder=asc"
     response = requests.get(url)
-    print(f"URL: {url}")
-    print(f"Response status code: {response.status_code}")
-    print(f"Response text: {response.text}")
     data = response.json()
 
     if "records" not in data or not data["records"]:
@@ -40,6 +39,7 @@ def scrape_painting():
         "artist": artist,
         "date": date
     }
+
 
 def generate_artwork_info(artist, title):
     prompt1 = f"You are an art critique and poet. Please provide a beautiful interpretation of '{title}' by {artist}. Make it short if possible in bullet points. Make it fun and interesting. Include a short section that explains how it could resonate with our current society."
@@ -87,6 +87,7 @@ def painting_of_the_day():
     painting["info"] = painting_info
     return render_template('index.html', painting=painting)
 
+
 @app.route('/next_artwork')
 def next_artwork():
     painting = scrape_painting()
@@ -94,7 +95,6 @@ def next_artwork():
     painting["info"] = painting_info
     return jsonify(painting)
 
+
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
