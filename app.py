a254c3e18ac0f74ddc33e545f473ea64b1d19d6d
Dylan B <dylan@MacBook-Pro-D-2.local>
2023-05-02 15:34:35 +0200

    fix13

--- app.py
+++ app.py
@@ -1,36 +1,68 @@
 from flask import Flask, render_template, jsonify
 import requests
-from bs4 import BeautifulSoup
 import openai
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-# Load OpenAI API key from environment variables
+# Load API keys from environment variables
 openai.api_key = os.environ.get('OPENAI_API_KEY')
+HARVARD_API_KEY = os.environ.get('HARVARD_API_KEY')
 
-def scrape_painting():
-    url = "https://www.harvardartmuseums.org/collections/object/299843"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
 
-    image_url = soup.find("img", class_="primary-image")["src"]
-    title = soup.find("h1", class_="title").text.strip()
-    artist = soup.find("div", class_="people-list__person").text.strip()
-    date = soup.find("span", class_="date-display-single").text.strip()
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    return {
-        "image_url": image_url,
-        "title": title,
-        "artist": artist,
-        "date": date
+
+@app.route('/get_painting')
+def get_painting():
+    painting = get_random_painting()
+    if painting:
+        painting_info = generate_artwork_info(painting["artist"], painting["title"])
+        painting["info"] = painting_info
+        return jsonify({"success": True, "data": painting})
+    else:
+        return jsonify({"success": False})
+
+
+def get_random_painting():
+    url = "https://api.harvardartmuseums.org/object"
+    params = {
+        "apikey": HARVARD_API_KEY,
+        "q": "classification:Paintings",
+        "size": 1,
+        "page": 1,
+        "sort": "random",
+        "sortorder": "asc",
+        "hasimage": 1,
     }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if data["info"]["totalrecords"] > 0:
+            painting = data["records"][0]
+            image_url = painting["primaryimageurl"]
+            title = painting["title"]
+            artist = painting["people"][0]["name"] if "people" in painting and painting["people"] else "Unknown artist"
+            date = painting["dated"]
+
+            return {
+                "image_url": image_url,
+                "title": title,
+                "artist": artist,
+                "date": date
+            }
+    return None
+
 
 def generate_artwork_info(artist, title):
-    prompt = f"Please provide a beautiful interpretation of '{title}' by {artist}. Make it short, fun, and interesting."
-
+    prompt = f"Provide a brief and interesting interpretation of '{title}' by {artist}."
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
@@ -42,19 +74,6 @@ def generate_artwork_info(artist, title):
 
     return response.choices[0].text.strip()
 
-@app.route('/')
-def index():
-    painting = scrape_painting()
-    painting_info = generate_artwork_info(painting["artist"], painting["title"])
-    painting["info"] = painting_info
-    return render_template('index.html', painting=painting)
-
-@app.route('/next_artwork')
-def next_artwork():
-    painting = scrape_painting()
-    painting_info = generate_artwork_info(painting["artist"], painting["title"])
-    painting["info"] = painting_info
-    return jsonify(painting)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
