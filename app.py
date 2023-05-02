bc9a82ba6a7273e15b05f1e1228e26cad3ab4624
Dylan B <dylan@MacBook-Pro-D-2.local>
2023-05-02 13:37:58 +0200

    fix

--- app.py
+++ app.py
@@ -5,15 +5,20 @@ import openai
 import random
 import os
 
+
 app = Flask(__name__)
 
 # Load OpenAI API key from environment variables
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
+
 def scrape_painting():
     api_key = os.environ.get("HARVARD_API_KEY")
     url = f"https://api.harvardartmuseums.org/object?apikey={api_key}&size=100&sort=random&classification=Paintings&hasimage=1&sortorder=asc"
     response = requests.get(url)
+    print(f"URL: {url}")
+    print(f"Response status code: {response.status_code}")
+    print(f"Response text: {response.text}")
     data = response.json()
 
     if "records" not in data or not data["records"]:
@@ -37,6 +42,7 @@ def scrape_painting():
         "artist": artist,
         "date": date
     }
+
 
 def generate_artwork_info(artist, title):
     prompt1 = f"You are an art critique and poet. Please provide a beautiful interpretation of '{title}' by {artist}. Make it short if possible in bullet points. Make it fun and interesting. Include a short section that explains how it could resonate with our current society."
@@ -76,6 +82,7 @@ def generate_artwork_info(artist, title):
         "article": response3.choices[0].text.strip()
     }
 
+
 @app.route('/')
 def index():
     painting = scrape_painting()
@@ -83,8 +90,15 @@ def index():
     painting["info"] = painting_info
     return render_template('index.html', painting=painting)
 
-@app.route('/')
-def artwork():
+
+@app.route('/background_image')
+def background_image():
+    painting = scrape_painting()
+    return jsonify({"image_url": painting["image_url"]})
+
+
+@app.route('/painting')
+def painting():
     painting = scrape_painting()
     painting_info = generate_artwork_info(painting["artist"], painting["title"])
     painting["info"] = painting_info
