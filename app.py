66d34d3694801587db771533d4651f12bee9226f
Dylan B <dylan@MacBook-Pro-D-2.local>
2023-05-02 12:22:48 +0200

    prompts

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ import openai
 import random
 import os
 
-
 app = Flask(__name__)
 
 # Load OpenAI API key from environment variables
 openai.api_key = os.environ.get("OPENAI_API_KEY")
-
 
 def scrape_painting():
     api_key = os.environ.get("HARVARD_API_KEY")
@@ -39,7 +37,6 @@ def scrape_painting():
         "artist": artist,
         "date": date
     }
-
 
 def generate_artwork_info(artist, title):
     prompt1 = f"You are an art critique and poet. Please provide a beautiful interpretation of '{title}' by {artist}. Make it short if possible in bullet points. Make it fun and interesting. Include a short section that explains how it could resonate with our current society."
@@ -79,14 +76,12 @@ def generate_artwork_info(artist, title):
         "article": response3.choices[0].text.strip()
     }
 
-
 @app.route('/')
 def painting_of_the_day():
     painting = scrape_painting()
     painting_info = generate_artwork_info(painting["artist"], painting["title"])
     painting["info"] = painting_info
     return render_template('index.html', painting=painting)
-
 
 @app.route('/next_artwork')
 def next_artwork():
@@ -95,6 +90,8 @@ def next_artwork():
     painting["info"] = painting_info
     return jsonify(painting)
 
-
 if __name__ == '__main__':
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
